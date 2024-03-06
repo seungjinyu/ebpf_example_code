31eae1e491a88059f71720554cd196ae9862d2ba
seungjinyu <seungjinyu93@gmail.com>
2024-03-06 12:59:04 +0900

    added some comment below hello-5.c

---
+++ c_ex/hello-1/hello-4.c
@@ -0,0 +1,28 @@
+/* 
+ * hello-4.c - Demonstrates module documentation. 
+ */ 
+#include <linux/init.h> /* Needed for the macros */ 
+#include <linux/module.h> /* Needed by all modules */ 
+#include <linux/printk.h> /* Needed for pr_info() */ 
+ 
+/*
+* Module licencs, author, description
+* They are defined within include/linux/module.h
+*/
+MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("LKMPG"); 
+MODULE_DESCRIPTION("A sample driver"); 
+ 
+static int __init init_hello_4(void) 
+{ 
+    pr_info("Hello, world 4\n"); 
+    return 0; 
+} 
+ 
+static void __exit cleanup_hello_4(void) 
+{ 
+    pr_info("Goodbye, world 4\n"); 
+} 
+ 
+module_init(init_hello_4); 
+module_exit(cleanup_hello_4);

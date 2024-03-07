438c243e31b11ec2ed4fdad2b26d15664457537c
seungjinyu <seungjinyu93@gmail.com>
2024-03-07 14:31:44 +0900

    ch hello-5.c, deleted comment

--- c_ex/hello-1/hello-5.c
+++ c_ex/hello-1/hello-5.c
@@ -67,14 +67,3 @@ static void __exit hello_5_exit(void)
 module_init(hello_5_init); 
 module_exit(hello_5_exit);
 
-/*
-
-sysfs 에 노출 되지 않으려면 권한 비트를 0 으로 설정해야 한다. 
-
-S_IRUSR: 소유자에 대한 읽기 권한 (User Read)
-S_IWUSR: 소유자에 대한 쓰기 권한 (User Write)
-S_IRGRP: 그룹에 대한 읽기 권한 (Group Read)
-S_IWGRP: 그룹에 대한 쓰기 권한 (Group Write)
-S_IROTH: 기타 사용자에 대한 읽기 권한 (Others Read)
-
-*/

1ad58ac46673eb5384344eb39e8d6027f814f378
seungjinyu <seungjinyu93@gmail.com>
2024-05-07 10:02:34 +0900

    added rust source code

---
+++ rust/slackcli/README.md
@@ -0,0 +1,4 @@
+#How to build
+
+cargo build -- release
+// the result will be in the target/release folder
--- rust/slackcli/src/main.rs
+++ rust/slackcli/src/main.rs
@@ -1,4 +1,4 @@
-use clap::Parser;
+use clap::{ Parser};
 
 
 #[derive(Parser,Debug)]
@@ -6,6 +6,7 @@ use clap::Parser;
 struct Args {
     #[arg(short , long )]
     name : String,
+    username: String,
 
     #[arg(short, long, default_value_t=1)]
     count: u8,
@@ -15,8 +16,31 @@ fn main() {
 
     let args = Args::parse();
 
+    
     for _ in 0..args.count{
         println!("Hello {}!", args.name);
     }
+
+    match args.name.as_str(){
+        "token"=>
+        {
+            if args.count < 1 {
+                    println!("Args is wrong please check again");
+                    println!("args count is {}",args.count);
+                    return;
+                }
+                let username = args.username;
+                token_function(username)
+        }
+        
+        _=> println!("nothing injected"),
+    }
+
     
+}
+
+fn token_function(username: String){
+
+    println!("Hello {}",username.as_str());
+
 }
